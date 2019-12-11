6ec20477418dcce76e5bf16f85626d419136e88e
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-12-11 22:16:13 +0700

    Deploy product

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from 'nestjs-config';
-import * as path from 'path';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
-  imports: [
-    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
-    TypeOrmModule.forRoot(typeOrmConfig),
-    ProductsModule,
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProductsModule, AuthModule],
   providers: [
     {
       provide: APP_FILTER,

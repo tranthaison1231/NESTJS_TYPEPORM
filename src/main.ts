a3b56d1ff90d387f7ec6299115889954a31bea43
tranthaison1231 <thaison.coderstokyo@gmail.com>
2020-08-12 18:20:37 +0700

    Update rate limit

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
     app.use(helmet());
     app.use(
       rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
+        windowMs: 5 * 60 * 1000, // 15 minutes
         max: 100, // limit each IP to 100 requests per windowMs
         message: '{"message": "Request so much. Let try after 15 minute."}',
         onLimitReached: (req) => {
